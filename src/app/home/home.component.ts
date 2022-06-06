a0528b386809d9d3edf58a02372845c9175221f7
Oscar Moran <lucmorang@outlook.com>
2022-06-06 13:37:48 -0500

    add new items into home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  panelOpenState = false;
+  color: ThemePalette = 'accent';
   constructor() { }
 
   ngOnInit(): void {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule } from '@angular/material/input';
+import {MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 @NgModule({
@@ -10,7 +14,10 @@ import {MatButtonModule} from '@angular/material/button';
   exports :[
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,MatSlideToggleModule
   ],
   imports: [
     CommonModule
